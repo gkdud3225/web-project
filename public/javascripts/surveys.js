af3d3c578c8b97c066d52d376c854afaf0d7b116
haYoungKim <adi0825@naver.com>
2015-12-10 04:44:16 +0900

    surveys.js, survey-main.jade modified

--- public/javascripts/surveys.js
+++ public/javascripts/surveys.js
@@ -1,152 +1,26 @@
-/*global $:false */
-/*global _:false */
-/*jslint browser:true, devel: true */
-var TaskController = function() {
-  function setAjaxHandler() {
-    $( document ).ajaxStart(function() {
-      $("#main").addClass("loading");
-    }).ajaxStop(function() {
-      $("#main").removeClass("loading");
-    });
-  }
+var tag1 = '<div class="input-group">질문 제목<input type="text" class="form-control input_text"></div><br/>';
+tag1 = tag1 + '<div class="row"><div class="col-lg-6" id="multi"><div class="input-group input_radio"><span class="input-group-addon"><input type="radio"></span><input type="text" class="form-control"></div><!-- /input-group --></div><!-- /.col-lg-6 --></div><br/><button class="btn btn-default" type="button" onClick=plus()>답변추가</button><hr/>';
 
-  function checked(type, value) {
-    var e = $("." + type + " .option[data-value='" + value + "']");
-    return e.hasClass('selected');
-  }
+var plus_tag1 = '<div class="input-group input_radio"><span class="input-group-addon"><input type="radio"></span><input type="text" class="form-control"></div>';
 
-  var Constructor = function () {
-    var self = this;
-    setAjaxHandler();
-    this.taskTemplate = _.template($("#task-template").html());
-    this.load();
-    $("#post-task").click(function() {
-      self.postTask();
-    }.bind(this));
-    $("section.options a.option")
-    .addClass('selected')
-    .click(function(e) {
-      $(e.currentTarget).toggleClass('selected');
-      self.render();
-    });
+var tag2 = '<div class="input-group">질문 제목<input type="text" class="form-control input_text"></div><br/>';
+tag2 = tag2 + '<div class="input-group">답변<input type="text" class="form-control input_text"></div><hr/>';
 
-    $("section.options a.all").click(function(e) {
-      var section = $($(e.currentTarget).closest('section'));
-      var options = section.find('.option');
-      if (options.length === section.find('.option.selected').length) {
-        options.removeClass('selected');
-      } else {
-        options.addClass('selected');
-      }
-      self.render();
-    });
-  };
+var tag3 = '<div class="input-group">질문 제목<input type="text" class="form-control input_text"></div><br/>';
+tag3 =  tag3 + '<div><textarea class="form-control input_text" rows="3">답변</textarea></div><hr/>';
 
-  Constructor.prototype._visible = function(task) {
-    if (!checked('done', task.done)) {
-      return false;
-    }
-    if (!checked('priority', task.priority)) {
-      return false;
-    }
-    if (_.includes(['개인', '가족', '업무'], task.category)) {
-      if (!checked('category', task.category)) {
-        return false;
-      }
-    } else if (!checked('category', '기타')) {
-      return false;
-    }
-    return true;
-  };
+$("#button_multiple").click(function() {
+	$("#mainform").append(tag1);
+});
 
-  Constructor.prototype.load = function() {
-    var self = this;
-    $.getJSON("/tasks", function(data) {
-      self.tasks = data;
-      self.render();
-      self.clearForm();
-    });
-  };
+$("#button_text").click(function() {
+	$("#mainform").append(tag2);
+});
 
-  Constructor.prototype.render = function() {
-    var self = this;
-    $("#main").toggleClass("no-task", (this.tasks.length <= 0));
-    var html = _.map(this.tasks, function(task) {
-      if (self._visible(task)) {
-        task.doneStr = task.done ? 'done' : '';
-        return self.taskTemplate(task);
-      }
-      return "";
-    });
-    $("ul.tasks").html(html.join("\n"));
-    $("ul.tasks .check").click(self.postDone.bind(this));
-    $(".task .remove").click(self.removeTask.bind(this));
-  };
+$("#button_longtext").click(function() {
+	$("#mainform").append(tag3);
+});
 
-  Constructor.prototype.clearForm = function() {
-    $("#form-task input").val("");
-    $("#form-task select[name='category']").val("개인");
-    $("#form-task select[name='priority']").val("2");
-    $("#form-task input:first").focus();
-  };
-
-  Constructor.prototype._findTask = function(e) {
-    var el = $(e.currentTarget).closest('li');
-    var id = el.data('id');
-    return  _.find(this.tasks, {id: id});
-  };
-
-  Constructor.prototype.postDone = function(e) {
-    var task = this._findTask(e);
-    if (!task) {
-      return;
-    }
-    var self = this;
-    $.ajax({
-      url: '/tasks/' + task.id,
-      method: 'PUT',
-      dataType: 'json',
-      data: {
-        done: task.done ? false : true
-      },
-      success: function(data) {
-        task.done = data.done;
-        self.render();
-      }
-    });
-  };
-
-  Constructor.prototype.postTask = function() {
-    var self = this;
-    $.post("/tasks", $("#form-task").serialize(), function(data) {
-      console.log(data);
-      self.tasks.push(data);
-      self.render();
-      self.clearForm();
-    });
-  };
-
-  Constructor.prototype.removeTask = function(e) {
-    var task = this._findTask(e);
-    if (!task) {
-      return;
-    }
-    var self = this;
-    if (confirm('정말로 삭제하시겠습니까?')) {
-      $.ajax({
-        url: '/tasks/' + task.id,
-        method: 'DELETE',
-        dataType: 'json',
-        success: function(data) {
-          self.tasks = _.reject(self.tasks, function(t) {
-            return t.id === task.id;
-          });
-          var el = $(e.currentTarget).closest('li');
-          el.remove();
-        }
-      });
-    }
-  };
-
-  return Constructor;
-} ();
+function plus(){
+  	$("#multi").append(plus_tag1);
+}
